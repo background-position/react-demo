bdb2618c271c90b2ed42d2ea7738be199d72b103
luojingwei <ljw@mobimedical.cn>
2023-10-10 14:02:08 +0800

    cc

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import Page1 from './page1';
-import Page2 from './page2';
-import Page3 from './page3';
-import Page4 from './page4';
-import Page5 from './page5';
-import Page6 from './page6';
-import './mock/data1';
-import './mock';
-import MyRedux from './mini-redux';
-
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Page1 from './page1'
+import Page2 from './page2'
+import Page3 from './page3'
+import Page4 from './page4'
+import Page5 from './page5'
+import Page6 from './page6'
+import Page7 from './page7'
+import MyRedux from './mini-redux'
 function App() {
-	return (
-		<MyRedux>
-			<BrowserRouter>
-				<Route path="/page1" component={Page1} />
-				<Route path="/page2" component={Page2} />
-				<Route path="/page3" component={Page3} />
-				<Route path="/page4" component={Page4} />
-				<Route path="/page5" component={Page5} />
-				<Route path="/page6" component={Page6} />
-			</BrowserRouter>
-		</MyRedux>
-	);
+  return (
+    <MyRedux>
+      <BrowserRouter>
+        <Route path="/page1" component={Page1} />
+        <Route path="/page2" component={Page2} />
+        <Route path="/page3" component={Page3} />
+        <Route path="/page4" component={Page4} />
+        <Route path="/page5" component={Page5} />
+        <Route path="/page6" component={Page6} />
+        <Route path="/page7" component={Page7} />
+      </BrowserRouter>
+    </MyRedux>
+  )
 }
 
-export default App;
+export default App
--- src/page2/index.tsx
+++ src/page2/index.tsx
@@ -3,23 +3,23 @@ import axios from 'axios';
 import request from 'umi-request';
 interface PageProps {}
 const App: FC<PageProps> = () => {
-	const getDat = async () => {
-		try {
-			let res = await axios.get(
-				'https://testvipoms.mobimedical.cn/api/vipoms/customerManager/getByIdCustomerProject',
-			);
-			console.log(res);
-			//	let res2 = await request.get('http://localhost:3000/#/getdata1');
-		} catch (error) {}
-	};
-	useEffect(() => {
-		getDat();
-	}, []);
-	return (
-		<div>
-			<h1 className="page-title">useEffect</h1>
-			<h1 className="result"></h1>
-		</div>
-	);
+    const getDat = async () => {
+        try {
+            let res = await axios.get('/api/users');
+            // let res2 = await request('/api/users/create', {
+            //     method: 'POST',
+            // });
+            console.log(res);
+        } catch (error) {}
+    };
+    useEffect(() => {
+        getDat();
+    }, []);
+    return (
+        <div>
+            <h1 className="page-title">useEffect</h1>
+            <h1 className="result"></h1>
+        </div>
+    );
 };
 export default App;
